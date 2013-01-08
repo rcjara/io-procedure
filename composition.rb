cb7104335a10408bca9db1a3712460a78a2b2c68
Raul Jara <raul.c.jara@gmail.com>
2013-01-08 14:51:32 -0500

    Add some headings.

--- composition.rb
+++ composition.rb
@@ -1,4 +1,9 @@
 compose do
   section file("sections/introduction")
+
+  section do
+    heading "Basic vocabulary"
+    text "In order to make sense of any description of the procedure, some basic vocabulary is necessary."
+  end
   section file("vocab/bea-benchmark-input-output-accounts")
 end
